931c6dd202c400d46f240846df33917da27992b8
hajohajo <joona.havukainen@helsinki.fi>
2019-11-13 22:17:56 +0200

    Osa2: Puhelinluettelo 2.6

---
+++ Osa2/puhelinluettelo/src/App.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+
+const App = () => {
+    const [persons, setPersons] = useState([
+        {name: 'Arto Hellas'}
+    ])
+
+    const [newName, setNewName] = useState('')
+
+    const handleNewName = (event) => {
+        console.log(event.target.value)
+        setNewName(event.target.value)
+    }
+
+    const addName = (event) => {
+        event.preventDefault()
+        const nameObject = {
+            name: newName
+        }
+        console.log({newName})
+        setPersons(persons.concat(nameObject))
+        setNewName('')
+        console.log({persons})
+    }
+
+    return (
+        <div>
+            {/*<div>debug: {persons.map(person => (person.name))}</div>*/}
+            <h2>Phonebook</h2>
+            <form onSubmit={addName}>
+                <div>
+                    name: <input value={newName} onChange={handleNewName}/>
+                </div>
+                <div>
+                    <button type="submit">add</button>
+                </div>
+            </form>
+            <h2>Numbers</h2>
+
+            {persons.map(person => <p>{person.name}</p>)}
+        </div>
+    )
+}
+
+export default App;
